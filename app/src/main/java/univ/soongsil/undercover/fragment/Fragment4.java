4ba0d772d3da9f717c3a6c23565ab6b4c0ef0a13
leego0221 <leego0221@naver.com>
2023-11-16 23:54:00 +0900

    feat: 설정 페이지 화면 구현

--- app/src/main/java/univ/soongsil/undercover/fragment/Fragment4.java
+++ app/src/main/java/univ/soongsil/undercover/fragment/Fragment4.java
@@ -15,7 +15,7 @@ public class Fragment4 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_setting_actvity, container, false);
+        return inflater.inflate(R.layout.activity_setting, container, false);
 
     }
 
