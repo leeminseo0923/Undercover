3c5068e9b1a1817e92c8bc698e178c9d8b02c24d
leego0221 <leego0221@naver.com>
2023-11-21 01:37:30 +0900

    feat: 성향 선택 페이지 image와 animation 포함하여 구현

--- app/src/main/java/univ/soongsil/undercover/fragment/BalanceGameFragment.java
+++ app/src/main/java/univ/soongsil/undercover/fragment/BalanceGameFragment.java
@@ -1,5 +1,7 @@
 package univ.soongsil.undercover.fragment;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,7 +14,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import univ.soongsil.undercover.BalanceGameActivity;
 import univ.soongsil.undercover.MainActivity;
@@ -26,6 +29,9 @@ public class BalanceGameFragment extends Fragment {
 
     /** 옵션 중 위의 항목을 선택하면 true, 아래의 항목을 선택하면 false로 저장 */
     static boolean[] travelOption = new boolean[5];
+    final String choiceUp = "choiceUp";
+    final String choiceDown = "choiceDown";
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,34 +47,38 @@ public class BalanceGameFragment extends Fragment {
         String[] arrayUp = getResources().getStringArray(R.array.option_array_up);
         String[] arrayDown = getResources().getStringArray(R.array.option_array_down);
 
-        binding.choiceUp.setText(arrayUp[count]);
-        binding.choiceDown.setText(arrayDown[count]);
+        // TODO: 사진 다 찾으면 사진 파일 이름 수정
+        binding.choiceUpImage.setImageResource(R.drawable.choice_mountain);
+        binding.choiceDownImage.setImageResource(R.drawable.choice_mountain);
 
-        binding.choiceUp.setOnClickListener(new View.OnClickListener() {
+        binding.choiceUpText.setText(arrayUp[count]);
+        binding.choiceDownText.setText(arrayDown[count]);
+
+        binding.choiceUpImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /** 선택 확인용 토스트 메시지 */
-                String chosen = binding.choiceUp.getText().toString();
-                Toast.makeText(getActivity().getApplicationContext(), chosen, Toast.LENGTH_SHORT).show();
+                startAnimations(choiceUp);
+
+                travelOption[count++] = true;
 
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        travelOption[count] = true;
-                        count++;
-
                         if (count == arrayUp.length) { // 모든 문항 수를 체크했으면 MainActivity로 이동
-                            /* TODO: 옵션 선택 확인용 토스트 메시지
-                            *   임의의 옵션 선택지 5개를 만들어 놓은 뒤 boolean[]으로 받음
-                            *   indent.putExtra()로 boolean[]을 보내긴 하지만, 아직 정확한 흐름이 나오지 않아서
-                            *   일단 BalanceGameFragment 내에서 값이 잘 받아졌는지 확인하는 토스트 메시지를 구성함
-                            * */
+                            /* TODO: BalanceGameFragment 내에서 값이 잘 받아졌는지 확인하는 토스트 메시지
+                             *   임의의 옵션 선택지 5개를 만들어 놓은 뒤 boolean[]으로 받음
+                             *   ex) "위, 위, 위, 아래, 아래"를 고르면 [true, true, true, false, false]가 저장됨
+                             *   모든 옵션 선택이 끝난 후 Logcat에서 확인할 수 있습니다.
+                             * */
                             for (int i = 0; i < travelOption.length; i++) {
                                 Log.d("tag", "옵션 " + (i + 1) + ": " + travelOption[i]);
-                            } // ex) "위, 위, 위, 아래, 아래"를 고르면 [true, true, true, false, false]가 저장됨
+                            }
+
+                            count = 0;
 
                             Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
-                            intent.putExtra("travelOption", travelOption);
+                            // TODO: DB에 해당 boolean[] 넣기
+                            // intent.putExtra("travelOption", travelOption);
                             startActivity(intent);
                             return;
                         }
@@ -78,30 +88,29 @@ public class BalanceGameFragment extends Fragment {
                         getActivity().finish();
                         getActivity().overridePendingTransition(0, 0);
                     }
-                }, 500);
+                }, 1000);
             }
         });
 
-        binding.choiceDown.setOnClickListener(new View.OnClickListener() {
+        binding.choiceDownImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /** 선택 확인용 토스트 메시지 */
-                String chosen = binding.choiceDown.getText().toString();
-                Toast.makeText(getActivity().getApplicationContext(), chosen, Toast.LENGTH_SHORT).show();
+                startAnimations(choiceDown);
+
+                travelOption[count++] = false;
 
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        travelOption[count] = false;
-                        count++;
+                        if (count == arrayDown.length) {
+                            for (int i = 0; i < travelOption.length; i++) {
+                                Log.d("tag", "옵션 " + (i + 1) + ": " + travelOption[i]);
+                            }
 
-                        for (int i = 0; i < travelOption.length; i++) {
-                            Log.d("tag", "옵션 " + (i + 1) + ": " + travelOption[i]);
-                        }
+                            count = 0;
 
-                        if (count == arrayDown.length) { // 모든 문항 수를 체크했으면 MainActivity로 이동
                             Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
-                            intent.putExtra("travelOption", travelOption);
+                            // intent.putExtra("travelOption", travelOption);
                             startActivity(intent);
                             return;
                         }
@@ -111,7 +120,7 @@ public class BalanceGameFragment extends Fragment {
                         getActivity().finish();
                         getActivity().overridePendingTransition(0, 0);
                     }
-                }, 500);
+                }, 1000);
             }
         });
     }
@@ -120,5 +129,59 @@ public class BalanceGameFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    /** 위 선택지를 골랐으면 choiceUp, 아래 선택지를 골랐으면 choiceDown을 인자로 주면 됨 */
+    public void startAnimations(String chosenOption) {
+        int pink = getResources().getColor(R.color.main_color, null);
+        int white = getResources().getColor(R.color.white, null);
+
+        // 배경 색 pink -> white
+        ValueAnimator backgroundColorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), pink, white);
+        backgroundColorAnimation.setDuration(600);
+        backgroundColorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                binding.balanceGameBackground.setBackgroundColor((int) animation.getAnimatedValue());
+            }
+        });
+        backgroundColorAnimation.start();
+
+        // 제목 글자와 선택한 옵션 글자 색 white -> pink
+        ValueAnimator textColorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), white, pink);
+        textColorAnimation.setDuration(600);
+        textColorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                binding.balanceGameTitle.setTextColor((int) animation.getAnimatedValue());
+
+                if (chosenOption.equals("choiceUp")) {
+                    binding.choiceUpText.setTextColor((int) animation.getAnimatedValue());
+                } else {
+                    binding.choiceDownText.setTextColor((int) animation.getAnimatedValue());
+                }
+            }
+        });
+        textColorAnimation.start();
+
+        // 선택한 옵션 중심으로 이동하면서 확대
+        if (chosenOption.equals("choiceUp")) {
+            Animation moveDownAnim = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.anim_move_down);
+            binding.choiceUp.startAnimation(moveDownAnim);
+        } else {
+            Animation moveUpAnim = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.anim_move_up);
+            binding.choiceDown.startAnimation(moveUpAnim);
+        }
+
+        // 나머지 선택지 fade out
+        Animation fadeOutAnim = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.anim_fade_out);
+        if (chosenOption.equals("choiceUp")) {
+            binding.choiceDown.startAnimation(fadeOutAnim);
+        } else {
+            binding.choiceUp.startAnimation(fadeOutAnim);
+        }
+
+        // VS 글자 fade out
+        binding.versus.startAnimation(fadeOutAnim);
     }
 }
