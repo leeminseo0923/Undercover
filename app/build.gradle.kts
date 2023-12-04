f1b6a33d41ff41468a1dcd31f5a371d85ad35837
leego0221 <leego0221@naver.com>
2023-12-04 16:45:32 +0900

    feat: build.gradle.kts에 recyclerview 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.material:material:1.2.0")
 
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
 
