253f76cdcfd2c52b5e8ca69c2dfd26b1a8a8b525
leeminseo0923 <64004511+leeminseo0923@users.noreply.github.com>
2023-12-05 00:26:53 +0900

    feat: 추천 장소 생성 기능 개발

--- app/src/main/java/univ/soongsil/undercover/repository/PlaceRepository.java
+++ app/src/main/java/univ/soongsil/undercover/repository/PlaceRepository.java
@@ -1,15 +1,19 @@
 package univ.soongsil.undercover.repository;
 
+import java.util.List;
+
 import univ.soongsil.undercover.domain.Coordinate;
+import univ.soongsil.undercover.domain.Place;
+import univ.soongsil.undercover.domain.Region;
 import univ.soongsil.undercover.domain.UpdateUI;
 import univ.soongsil.undercover.domain.User;
 
 public interface PlaceRepository {
     void updateWeight(String placeName, User user, Integer rate);
 
-    void addPlace(String placeName, Coordinate coordinate);
-
-    void addPlace(String placeName, Double longitude, Double latitude);
+    void addPlace(Place place);
 
     void getLocation(String placeName, UpdateUI<Coordinate> updateUI);
+
+    void getBestPlaces(Region region, List<Boolean> options, UpdateUI<List<String>> updateUI);
 }
--- app/src/main/java/univ/soongsil/undercover/repository/PlaceRepositoryImpl.java
+++ app/src/main/java/univ/soongsil/undercover/repository/PlaceRepositoryImpl.java
@@ -1,10 +1,22 @@
 package univ.soongsil.undercover.repository;
 
 import android.util.Log;
+import android.util.Pair;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import univ.soongsil.undercover.domain.Coordinate;
+import univ.soongsil.undercover.domain.Place;
+import univ.soongsil.undercover.domain.Region;
 import univ.soongsil.undercover.domain.Restaurant;
 import univ.soongsil.undercover.domain.Sight;
 import univ.soongsil.undercover.domain.UpdateUI;
@@ -26,17 +38,9 @@ public abstract class PlaceRepositoryImpl implements PlaceRepository{
     }
 
     @Override
-    public void addPlace(String placeName, Coordinate coordinate) {
-        Sight sight = new Sight(placeName, coordinate);
-        reference.document(placeName)
-                .set(sight);
-    }
-
-    @Override
-    public void addPlace(String placeName, Double longitude, Double latitude) {
-        Restaurant restaurant = new Restaurant(placeName, longitude, latitude);
-        reference.document(placeName)
-                .set(restaurant);
+    public void addPlace(Place place) {
+        reference.document(place.getName())
+                .set(place);
     }
 
     @Override
@@ -53,5 +57,28 @@ public abstract class PlaceRepositoryImpl implements PlaceRepository{
                     }
                 })
                 .addOnFailureListener(e -> Log.e(COLLECTION, "Fail to get location in " + placeName + " " + e.getMessage()));
+    }
+
+    @Override
+    public void getBestPlaces(Region region, List<Boolean> options, UpdateUI<List<String>> updateUI) {
+        reference.get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    List<Pair<String, Double>> places = new ArrayList<>();
+                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        Place place = documentSnapshot.toObject(Place.class);
+                        Double sum = 0.0;
+                        for (int i = 0; i < options.size(); i++) {
+                            if (options.get(i)) {
+                                sum += place.getWeights().get(i);
+                            }
+                        }
+                        places.add(new Pair<>(place.getName(), sum));
+                    }
+
+                    places.sort((o1, o2) -> (int) (o1.second - o2.second));
+
+                    updateUI.onSuccess(places.stream().map(stringDoublePair -> stringDoublePair.first).collect(Collectors.toList()));
+                })
+                .addOnFailureListener((e)->updateUI.onFail());
     }
 }
